28074a79ea4bb2a8eb3fe0bbe491c0d51ff45de5
Yash Modha <modhayash6@gmail.com>
2021-07-03 15:05:59 -0700

    Initial Commit

---
+++ src/main/groovy/com/modhayash/ReadmeApplication.groovy
@@ -0,0 +1,15 @@
+package com.modhayash
+
+import groovy.util.logging.Slf4j
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@Slf4j
+@SpringBootApplication
+class ReadmeApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run ReadmeApplication, args
+	}
+}
